c4d3a25c2db0bbc68a7805928ebb30decaeec400
Lisandro Arroyo <Arroyolisandro03@gmail.com>
2023-12-17 21:51:26 -0300

    correcciones

--- recursos/js/catalogo.js
+++ recursos/js/catalogo.js
@@ -5,7 +5,7 @@ const catalogo = {
             id_articulo: 1,
             nombre: "CAMPO VIEJO",
             tipo: "Malbec",
-            precio: $2500,
+            precio: "$2500",
             imagen: "./recursos/imagenes/catalogo/campo-viejo.jpg",
             descripcion: "",
             medidas:[{medida: "1L", stock: 20},{medida: "3/4", stock: 5}]
@@ -16,7 +16,7 @@ const catalogo = {
             id_articulo: 2,
             nombre: "CHÂTEAU MUSAR",
             tipo: "Malbec",
-            precio: 7780,
+            precio: "$7780",
             imagen: "./recursos/imagenes/catalogo/chateau.musar.jpg",
             descripcion: "",
             medidas:[{medida: "1L", stock: 20},{medida: "3/4", stock: 5}]
@@ -28,7 +28,7 @@ const catalogo = {
             id_articulo: 3,
             nombre: "PLANETA",
             tipo: "Malbec",
-            precio: 2500,
+            precio: "$2500",
             imagen: "./recursos/imagenes/catalogo/planeta.jpg",
             descripcion: "",
             medidas:[{medida: "1L", stock: 20},{medida: "3/4", stock: 5}]
@@ -40,7 +40,7 @@ const catalogo = {
             id_articulo: 4,
             nombre: "RUTINI",
             tipo: "Malbec",
-            precio: 8500,
+            precio: "$8500",
             imagen: "./recursos/imagenes/catalogo/rutini.jpg",
             descripcion: "",
             medidas:[{medida: "1L", stock: 20},{medida: "3/4", stock: 5}]
@@ -52,8 +52,8 @@ const catalogo = {
             id_articulo: 5,
             nombre: "VILLA MARIA",
             tipo: "Malbec",
-            precio: 1100,
-            imagen: "./recursos/imagenes/catalogo/villa-maria.jpg",
+            precio: "$1100",
+            imagen: "./recursos/imagenes/catalogo/villa-maria.jpeg",
             descripcion: "",
             medidas:[{medida: "1L", stock: 20},{medida: "3/4", stock: 5}]
 
@@ -63,8 +63,8 @@ const catalogo = {
             id_articulo: 6,
             nombre: "19 CRIMES",
             tipo: "Malbec",
-            precio: 1100,
-            imagen: "./recursos/imagenes/catalogo/19-crimes.jpg",
+            precio: "$1100",
+            imagen: "./recursos/imagenes/catalogo/19-crimes.jpeg",
             descripcion: "",
             medidas:[{medida: "1L", stock: 20},{medida: "3/4", stock: 5}]
 
